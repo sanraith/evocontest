d75887c8470bc9bb58ddb61b7d34107f8abfb8f3
Soma Zsják <zsjaks@gmail.com>
2019-06-10 20:40:26 +0200

    Added project to test performance measurements.

--- LimiterTest/Program.cs
+++ LimiterTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,12 +9,21 @@ namespace LimiterTest
     {
         static void Main(string[] args)
         {
+            var a = 0;
+            var b = 0;
+            (a, b) = (b, a);
+
             Task.Run(async () => await MainAsync(args)).GetAwaiter().GetResult();
         }
 
         private static async Task MainAsync(string[] args)
         {
-            for (int i = 0; i < 10; i++)
+            if (!int.TryParse(args.FirstOrDefault(), out var repeatCount))
+            {
+                repeatCount = 10;
+            }
+
+            for (int i = 0; i < repeatCount; i++)
             {
                 var spinner = new Spinner();
                 var count = await spinner.Run();
---
+++ PerformanceTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PerformanceTest
+{
+    internal sealed class PerformanceTestProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
