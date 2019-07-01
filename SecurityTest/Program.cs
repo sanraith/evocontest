9d56137ce80527d9c092ccbafd40c410f9d29f3e
Soma Zsják <zsjaks@gmail.com>
2019-07-01 23:15:02 +0200

    Updated architecture doc. Updated SecurityTest with checks.

--- SecurityTest/Program.cs
+++ SecurityTest/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Net;
 
 namespace SecurityTest
 {
@@ -21,36 +22,51 @@ namespace SecurityTest
         }
     }
 
-    public sealed class Sandboxed
-    {
-        public void Run()
-        {
-            try
-            {
-                using (var s = File.CreateText("test.txt"))
-                {
-                    s.WriteLine("Hello");
-                }
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Could not create new file");
-            }
-            try
-            {
-                File.WriteAllText("output.txt", "hello");
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Could not write to output");
-            }
-
-        }
-    }
 
     public sealed class Launcher
     {
         public void Run()
         { }
+    }
+
+    public sealed class Sandboxed
+    {
+        public void Run()
+        {
+            var isSandbox = true;
+            isSandbox &= AssertDenied("read from outside of sandbox", () =>
+            {
+                var secrets = File.ReadAllText("~/secrets.txt");
+            });
+            isSandbox &= AssertDenied("create new file", () =>
+            {
+                using (var s = File.CreateText("test.txt"))
+                {
+                    s.WriteLine("Hello");
+                }
+            });
+            isSandbox &= AssertDenied("access to the internet", () =>
+            {
+                WebClient client = new WebClient();
+                string downloadString = client.DownloadString("http://www.gooogle.com");
+            });
+
+            Console.WriteLine($"Is sandbox: {isSandbox}");
+        }
+
+        public bool AssertDenied(string desc, Action a)
+        {
+            try
+            {
+                a();
+                Console.WriteLine($"ERROR - no exception for {desc}");
+                return false;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"OK - {desc} not allowed.");
+                return true;
+            }
+        }
     }
 }
--- docs/architecture.md
+++ docs/architecture.md
@@ -3,7 +3,7 @@
 ## Introduction
 This document contains information about the general architecture of the components required to run the contest.
 
-## Overview
+# Overview
 ```mermaid
 graph LR
 
@@ -28,6 +28,8 @@ BE --> DB
 BE --> FS
 RPI --> BE
 ```
+
+# Web
 
 ## Submitting a solution
 
@@ -57,4 +59,88 @@ loop long polling
 	BE ->> DB: Save results
 end
 BE -->> client: Push test results (slow)
+```
+
+# Raspberry
+```mermaid
+graph TD
+
+subgraph Azure
+	cloud[evowar BE]
+end
+
+subgraph Raspberry
+	host[Runner.Host]
+	subgraph Sandbox
+		slave[Runner.Slave]
+		subgraph Assembly Load Context
+			dll[Submitted library]
+		end
+	end
+end
+
+host -->|authenticated http| cloud
+host -->|named pipe| slave
+slave --> dll
+```
+
+## Run submission checks
+
+```mermaid
+sequenceDiagram
+
+participant cloud as evowar BE
+participant host as Runner host
+participant slave as Sandbox worker
+participant sub as Submission library
+
+host ->> cloud: Download submissions
+loop for each Submission
+	host ->> slave: Create
+	activate slave
+	Note over host, slave: Named pipe connection
+
+	slave ->> sub: Load to context
+	activate sub
+	slave -->> sub: Static analysis
+	slave -->> host: Static analysis results
+	slave ->> sub: Unit tests
+	deactivate sub
+	slave -->> host: Test results
+	deactivate slave
+end
+host ->> cloud: Upload results
+```
+
+## Run performance measurements
+
+```mermaid
+sequenceDiagram
+
+participant host as Runner host
+participant fs as Filesystem
+participant slave as Sandbox worker
+participant sub as Submission library
+
+loop while avg runtime &lt; 100ms
+	opt no input available
+		slave -->> host: Status message
+		slave ->> slave: Generate harder input
+		slave ->> fs: Save input
+		Note over fs: Encrypted files
+	end
+	slave ->> fs: Load input
+	loop n performance runs
+		slave ->> sub: Load to context
+		activate sub
+		slave ->> sub: Warmup
+		Note right of sub: easy input
+		slave ->> sub: Measure
+		Note right of sub: hard input
+		deactivate sub
+		slave ->> slave: validate results
+		slave -->> host: Partial results
+	end
+end
+slave -->> host: Total results
 ```
