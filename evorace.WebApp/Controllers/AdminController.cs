303b8c72fcfb489b094d0967f5f4345642e7e561
Soma Zsják <zsjaks@gmail.com>
2019-07-12 22:08:04 +0200

    Added admin button to delete submissions.

--- evorace.WebApp/Controllers/AdminController.cs
+++ evorace.WebApp/Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using evorace.WebApp.Data;
 using evorace.WebApp.Data.Helper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -11,9 +12,27 @@ namespace evorace.WebApp.Controllers
     [Authorize(Roles = Roles.Administrator)]
     public class AdminController : Controller
     {
+        public AdminController(ContestDb db)
+        {
+            myDb = db;
+        }
+
         public IActionResult Admin()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> DoClearAllSubmissions()
+        {
+            myDb.Submissions.RemoveRange(myDb.Submissions);
+            await myDb.SaveChangesAsync();
+
+            ViewBag.Message = "Success";
+
+            return View(nameof(Admin));
+        }
+
+        private readonly ContestDb myDb;
     }
 }
