8be66ab400cb78a78d50eb58b895607f6e618e25
Soma Zsják <zsjaks@gmail.com>
2019-07-28 15:31:58 +0200

    Do not allow changing email address in webapp.

--- evorace.WebApp/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
+++ evorace.WebApp/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -69,41 +69,41 @@ namespace evorace.WebApp.Areas.Identity.Pages.Account.Manage
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
+        //public async Task<IActionResult> OnPostAsync()
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return Page();
+        //    }
 
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
+        //    var user = await _userManager.GetUserAsync(User);
+        //    if (user == null)
+        //    {
+        //        return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+        //    }
 
-            var email = await _userManager.GetEmailAsync(user);
-            if (Input.NewEmail != email)
-            {
-                var userId = await _userManager.GetUserIdAsync(user);
-                var code = await _userManager.GenerateChangeEmailTokenAsync(user, Input.NewEmail);
-                var callbackUrl = Url.Page(
-                    "/Account/ConfirmEmailChange",
-                    pageHandler: null,
-                    values: new { userId = userId, email = Input.NewEmail, code = code },
-                    protocol: Request.Scheme);
-                await _emailSender.SendEmailAsync(
-                    Input.NewEmail,
-                    "Confirm your email",
-                    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+        //    var email = await _userManager.GetEmailAsync(user);
+        //    if (Input.NewEmail != email)
+        //    {
+        //        var userId = await _userManager.GetUserIdAsync(user);
+        //        var code = await _userManager.GenerateChangeEmailTokenAsync(user, Input.NewEmail);
+        //        var callbackUrl = Url.Page(
+        //            "/Account/ConfirmEmailChange",
+        //            pageHandler: null,
+        //            values: new { userId = userId, email = Input.NewEmail, code = code },
+        //            protocol: Request.Scheme);
+        //        await _emailSender.SendEmailAsync(
+        //            Input.NewEmail,
+        //            "Confirm your email",
+        //            $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                StatusMessage = "Confirmation link to change email sent. Please check your email.";
-                return RedirectToPage();
-            }
+        //        StatusMessage = "Confirmation link to change email sent. Please check your email.";
+        //        return RedirectToPage();
+        //    }
 
-            StatusMessage = "Your email is unchanged.";
-            return RedirectToPage();
-        }
+        //    StatusMessage = "Your email is unchanged.";
+        //    return RedirectToPage();
+        //}
 
         public async Task<IActionResult> OnPostSendVerificationEmailAsync()
         {
