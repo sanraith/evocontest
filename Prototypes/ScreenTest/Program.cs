fa8ada463e33fd40f73f87b5c83952540e18b0de
Soma Zsják <zsjaks@gmail.com>
2019-08-01 21:48:24 +0200

    Added prototype for writing to the raspberry e-paper screen.

---
+++ Prototypes/ScreenTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScreenTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var screenHandler = new CustomScreenHandler();
+            await screenHandler.InitializeAsync();
+
+            args = args.Any() ? args : new[] { string.Empty };
+            switch (args[0])
+            {
+                case "draw": await screenHandler.DrawSomethingAsync(); break;
+                case "clear":
+                default:
+                    await screenHandler.ClearAsync();
+                    break;
+            }
+        }
+    }
+}
